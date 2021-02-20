57c05117d25ba9299458840258c17024ef1bcd67
7imaZz <abdelrahimelshorbgy@gmail.com>
2021-02-20 12:13:48 +0200

    Design Login & Register Activities

--- app/src/main/java/com/shorbgy/ecommerceapp/MainActivity.java
+++ app/src/main/java/com/shorbgy/ecommerceapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.shorbgy.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.shorbgy.ecommerceapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.loginBtn.setOnClickListener(v ->
+                startActivity(new Intent(this, LoginActivity.class)));
+
+        binding.joinNowBtn.setOnClickListener(v->
+                startActivity(new Intent(this, RegisterActivity.class)));
     }
 }
