54d4611a08e57854f2d42f1f24390bc7d39e868c
7imaZz <abdelrahimelshorbgy@gmail.com>
2021-02-26 10:36:58 +0200

    Allow Admin to Delete Existing Product

--- app/src/main/java/com/shorbgy/ecommerceapp/ui/AdminItemFragment.java
+++ app/src/main/java/com/shorbgy/ecommerceapp/ui/AdminItemFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,8 @@ import com.shorbgy.ecommerceapp.R;
 import com.shorbgy.ecommerceapp.databinding.FragmentAdminItemBinding;
 import com.shorbgy.ecommerceapp.pojo.Product;
 import com.shorbgy.ecommerceapp.utils.Constants;
+import com.squareup.picasso.Picasso;
+
 import java.util.HashMap;
 import java.util.Objects;
 
@@ -52,11 +55,16 @@ public class AdminItemFragment extends Fragment {
         binding.productNam.setText(product.getProduct_name());
         binding.productPrice.setText(product.getPrice());
         binding.productDesc.setText(product.getDescription());
+        Picasso.get()
+                .load(product.getImage_url())
+                .into(binding.productImage);
 
         binding.updateTv.setOnClickListener(v -> updateProduct());
 
         binding.closeTv.setOnClickListener(v ->
                 Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).popBackStack());
+
+        binding.deleteButton.setOnClickListener(v -> deleteProduct());
     }
 
     private void updateProduct(){
@@ -74,5 +82,24 @@ public class AdminItemFragment extends Fragment {
                 Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).popBackStack();
             }
         });
+    }
+
+    private void deleteProduct(){
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(requireContext());
+
+        dialog.setTitle("Confirmation");
+        dialog.setMessage("Are you sure you want to delete this product?");
+
+        dialog.setPositiveButton("Yes", (dialog1, which) -> databaseReference.child(product.getPid()).removeValue().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Snackbar.make(requireView(), "Product Deleted Successfully", Snackbar.LENGTH_SHORT).show();
+                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).popBackStack();
+            }
+        }));
+
+        dialog.setNegativeButton("Cancel", (dialog12, which) -> dialog12.dismiss());
+
+        dialog.show();
     }
 }
